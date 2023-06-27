176933c2d418189ca88ac73b6311d9e01f2f48bb
Your Name <you@example.com>
2023-06-27 15:45:10 +0530

    Using relations and connected cart and user

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
-// const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -22,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('64994adb836c12c399610e95')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user
       next();
     })
     .catch(err => console.log(err));
@@ -33,8 +32,20 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://dipankarzeus10:Mylovemom10@cluster0.v2es2ck.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://dipankarzeus10:Mylovemom10@cluster0.v2es2ck.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
+    User.findOne().THEN(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Dipankar',
+          email: 'dipankar@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save()
+      }
+    })
     app.listen(3000)
   }).catch(err => {
     console.log(err)
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,9 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find() // mongoose find do not return curson it returns the products
     .then(products => {
+      console.log(products)
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
@@ -16,16 +17,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
-  Product.findById(prodId)
+  Product.findById(prodId) // mongoose has finebyid function predefined
     .then(product => {
       res.render('shop/product-detail', {
         product: product,
@@ -37,7 +29,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
@@ -52,18 +44,15 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(products => {
+      console.log(products)
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
@@ -72,52 +61,17 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId).then(product => {
     return req.user.addToCart(product);
+
   }).then(result => {
     console.log(result)
-  })
-  //   let fetchedCart;
-  //   let newQuantity = 1;
-  //   req.user
-  //     .getCart()
-  //     .then(cart => {
-  //       fetchedCart = cart;
-  //       return cart.getProducts({ where: { id: prodId } });
-  //     })
-  //     .then(products => {
-  //       let product;
-  //       if (products.length > 0) {
-  //         product = products[0];
-  //       }
-
-  //       if (product) {
-  //         const oldQuantity = product.cartItem.quantity;
-  //         newQuantity = oldQuantity + 1;
-  //         return product;
-  //       }
-  //       return Product.findById(prodId);
-  //     })
-  //     .then(product => {
-  //       return fetchedCart.addProduct(product, {
-  //         through: { quantity: newQuantity }
-  //       });
-  //     })
-  //     .then(() => {
-  //       res.redirect('/cart');
-  //     })
-  //     .catch(err => console.log(err));
+    res.redirect('/cart');
+  });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
+    .deleteItemFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -127,27 +81,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   let fetchedCart;
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch(err => console.log(err));
-    })
-    .then(result => {
-      return fetchedCart.setProducts(null);
-    })
+    .addOrder()
     .then(result => {
       res.redirect('/orders');
     })
